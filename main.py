a36cd071a944b43bed8072729f519019f754be49
J. Felipe Rodrigues <joaofelipecrodrigues@gmail.com>
2023-01-29 12:34:49 -0300

    :art: new banner

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import tkinter
+
+# Collors ---------------------------
+back = '#333652'
+black = "black"
+white = 'white'
+yellow = '#FAD02C'
+green = '#417B5A'
+purple = '#BA6E6E'
+blue = '#8EB1C7'
+
+# Fonts -----------------------------
+font_title = 'Roboto 18'
+font_obj = 'Roboto 1'
+font_label = 'Roboto 12'
+font_button = 'Roboto 13'
+font_button_ext = 'Roboto 10'
