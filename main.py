aa7ef6be6818de44b3267e892c3929a1fb700635
J. Felipe Rodrigues <joaofelipecrodrigues@gmail.com>
2023-01-31 23:07:52 -0300

    :sparkles: star of sign-up

--- main.py
+++ main.py
@@ -16,6 +16,22 @@ font_label = 'Roboto 12'
 font_button = 'Roboto 13'
 font_button_ext = 'Roboto 10'
 
+class Registration(Tk):
+    def __init__(self, geometry, width=False, height=False, bg=back):
+        super().__init__()
+
+        self.title("QAgenda")
+        self.geometry(geometry)
+        self.config(bg=bg)
+        self.resizable(width=width, height=height)
+
+    def widgets_sub(self):
+        # Frames
+        self.frame_1 = Frame(self, width=350, height=50, background=black, relief='flat')
+        self.frame_2 = Frame(self, width=350, height=300, background=black, relief='flat')
+
+
+
 class Login(Tk):
     def __init__(self, geometry, width=False, height=False, bg=back):
         super().__init__()
@@ -41,8 +57,8 @@ class Login(Tk):
         self.entry_pass = Entry(self.frame_2, width=23, bg=white, fg=black, font=(font_label), highlightthickness=1, show='*', relief='flat')
         
         # Buttons
-        self.button_confirm = Button(self.frame_2, text="Entrar", width=10, height=1, relief='flat', font=(font_button), highlightthickness=1, bg=button_collor, fg=back)
-        self.button_inscr = Button(self.frame_2, text="Cadastrar", width=10, anchor=N, font=(font_button_ext), highlightthickness=-1, bg=back, fg=collor_font, relief='flat')
+        self.button_confirm = Button(self.frame_2, text="Entrar",width=10, height=1, relief='flat', font=(font_button), highlightthickness=1, bg=button_collor, fg=back)
+        self.button_inscr = Button(self.frame_2, text="Cadastrar", command=self.new_window_sub, width=10, anchor=N, font=(font_button_ext), highlightthickness=-1, bg=back, fg=collor_font, relief='flat')
         self.button_lost = Button(self.frame_2, text="Esqueceu a Senha?", width=15, anchor=N, font=(font_button_ext), highlightthickness=-1, bg=back, fg=collor_font, relief='flat')
         
 
@@ -67,7 +83,13 @@ class Login(Tk):
         self.button_confirm.place(x=115, y=150)
         self.button_inscr.place(x=38, y=200)
         self.button_lost.place(x=172, y=200)
+    
+    def new_window_sub(self):
+        self.destroy()
+        subs = Registration("700x473")
+        subs.mainloop()
 
+# ----------- Main ----------------
 login = Login("350x350")
 login.widgets_init_sigin()
 
