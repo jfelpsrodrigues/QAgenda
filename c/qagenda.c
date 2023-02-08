e328a966bd8d76b0b3db4de00c5e1f8d3c24d570
benjamimarthur <117108311+benjamimarthur@users.noreply.github.com>
2023-02-08 14:09:12 -0300

    Função cadastro de lojas

--- c/qagenda.c
+++ c/qagenda.c
@@ -4,7 +4,18 @@
 
 void CadastroLoja();
 
-void CadastroCliente();
+void CadastroCliente(List *t,char *nome,char *bairro, int *senha, long int *cpf_cnpj, char *ramo) {
+    Cad a;
+    a.nome = nome;
+    a.bairro = bairro;
+    a.senha = senha;
+    a.cpf_cnpj = cpf_cnpj;
+    a.ramo = ramo;
+   
+
+    addFim(t, a);
+    printf("\n");
+}
 
 void RealizarAgendamento();
 
